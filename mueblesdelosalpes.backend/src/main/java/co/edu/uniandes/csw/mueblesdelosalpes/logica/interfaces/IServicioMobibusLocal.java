a9fb043936b11b7e7236e392eba5d7c768b4780e
pa.sarmiento10 <pa.sarmiento10@ISIS2503-36.virtual.uniandes.edu.co>
2015-08-16 17:20:26 -0500

    mobibus2

---
+++ mueblesdelosalpes.backend/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/logica/interfaces/IServicioMobibusLocal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mueblesdelosalpes.logica.interfaces;
+
+import co.edu.uniandes.csw.mueblesdelosalpes.dto.Mobibus;
+import co.edu.uniandes.csw.mueblesdelosalpes.dto.Tranvia;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author pa.sarmiento10
+ */
+
+@Local
+
+public interface IServicioMobibusLocal {
+     public List<Mobibus> darMobibuses();
+}
