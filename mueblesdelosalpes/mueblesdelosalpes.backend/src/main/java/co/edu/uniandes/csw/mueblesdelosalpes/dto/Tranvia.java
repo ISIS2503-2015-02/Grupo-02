18517c522437e6429b2a9863c4d82d70509b0830
pacho950609 <franciscoricaurte-95@hotmail.com>
2015-08-16 13:48:53 -0500

    Tranvia

---
+++ mueblesdelosalpes/mueblesdelosalpes.backend/src/main/java/co/edu/uniandes/csw/mueblesdelosalpes/dto/Tranvia.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mueblesdelosalpes.dto;
+
+/**
+ *
+ * @author fj.ricaurte10
+ */
+public class Tranvia {
+    
+    private String nombre ;
+    
+    private String linea ;
+    
+    private String posicion ;
+    
+    private int nivelChoque;
+    
+    private int nivelTemperatura;
+    
+    private int nivelPanico ;
+    
+    
+    public Tranvia(String nombreP, String lenaP,String posicion,int nivelChoqueP,int nivelTemperaturaP, int nivelPanicoP)
+    {
+     
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
